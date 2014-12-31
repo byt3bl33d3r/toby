a2b3ec7a6cd0e243ed4bbb97db39012a504bed37
byt3bl33d3r <byt3bl33d3r@gmail.com>
2014-12-31 17:53:13 +0100

    added ignore case option

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
-toby
+Toby
 ====
-When reverse engineering firmware of embedded systems, often just searching for a specific string using ```grep``` and/or ```strings``` on a binary or file can reveal potential vulnerabilities.
-This tool simply aims to automate this process.
 
-Requires the ```grep```, ```cat```, ```strings```, ```file``` unix utilities and the python magic library to be installed
+When reverse engineering firmware of embedded systems, often just searching for a specific string using ```grep``` on a file or ```strings``` on a binary can reveal potential vulnerabilities.
+This tool simply aims to automate this process by identifying the filetype and running the appropriete commands.
+
+Requires the ```grep```, ```strings```and ```file``` unix utilities
+
+Requires the following python libraries:
+- magic
+- termcolor
--- toby.py
+++ toby.py
@@ -3,9 +3,11 @@
 import os
 import magic
 import argparse
+from termcolor import colored
 from subprocess import Popen, PIPE
 
-parser = argparse.ArgumentParser(description='Toby', epilog='Here you are, doggy! Good old Toby! Smell it, Toby, smell it!')
+parser = argparse.ArgumentParser(description='Toby', epilog='Yes, a queer mongrel, with a most amazing power of scent')
+parser.add_argument('-i', dest='case', action='store_true', default=False, help='Ignore case distinctions')
 parser.add_argument('-s', dest='string', metavar='string', required=True, type=str, help='String to search for')
 parser.add_argument('-d', dest='directory', metavar='directory', required=True, type=str, help='Directory to search')
 
@@ -26,16 +28,25 @@ for root, dirs, files in os.walk(args.directory):
 				with magic.Magic(flags=magic.MAGIC_MIME_ENCODING) as m:
 					encoding = m.id_filename(filepath)
 
+				grep_args = ["grep", "--color=always"]
+				strings_args = ["strings"]
+
+				if args.case:
+					grep_args.append('-i')
+
 				#we will ignore symlinks since most of them will be broken anyway
 				if 'inode/symlink' not in mime:
 					if 'binary' in encoding:
-						p1 = Popen(["strings", filepath], stdout=PIPE)
+						strings_args.append(filepath)
+						grep_args.append(args.string)
+						p1 = Popen(strings_args , stdout=PIPE)
+						p2 = Popen(grep_args, stdin=p1.stdout, stdout=PIPE)
+						p1.stdout.close()
 					else:
-						p1 = Popen(["cat", filepath], stdout=PIPE)
+						grep_args.extend(["-n", args.string, filepath])
+						p2 = Popen(grep_args, stdout=PIPE)
 
-					p2 = Popen(["grep", args.string], stdin=p1.stdout, stdout=PIPE)
-					p1.stdout.close()
 					output = p2.communicate()[0]
 					if output:
-						print filepath, mime, encoding
+						print colored(filepath, 'magenta', attrs=['bold']),  colored(mime, 'blue'), colored(encoding, 'blue')
 						print output
